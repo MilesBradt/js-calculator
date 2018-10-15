bdc5e4806a2263f165f1401b8e103f82b3cb82bb
Michael Bradt, Maly Phongsavanh and Victoria Martinez <victoria@>
2018-10-15 12:43:53 -0700

    output text in div rather than alert box

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+
+// User input without jquery
+
 // var userMass = parseInt(prompt("Enter your weight(lbs): "));
 // var userHeight = parseInt(prompt("Enter your height(inches): "));
 
-
+// Write a function that calculates Body Mass Index
 var bmiIndex = function(mass, height) {
   return mass / (height**2) * 703;
 };
+
+//Write a function that converts temperature between celsius and fahrenheit
+
+
+
+//Write functions to be used by a cooking app that convert between units
+
 
 
 $(document).ready(function() {
@@ -12,6 +22,7 @@ $(document).ready(function() {
     event.preventDefault();
     var mass = parseInt($("#userMass").val());
     var height = parseInt($("#userHeight").val());
-    alert(bmiIndex(mass, height));
+    // alert(bmiIndex(mass, height));
+    $('.output').text("Your BMI is " + bmiIndex(mass, height));
   });
 });
