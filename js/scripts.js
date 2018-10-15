8f529e28a5a673466fde41db1002ff848c49a30c
Michael Bradt, Maly Phongsavanh and Victoria Martinez <maly@>
2018-10-15 14:01:40 -0700

    remove spacing and use proper indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,22 @@
-
-// User input without jquery
-
-// var userMass = parseInt(prompt("Enter your weight(lbs): "));
-// var userHeight = parseInt(prompt("Enter your height(inches): "));
+// Business Logic
 
 // Write a function that calculates Body Mass Index
 var bmiIndex = function(mass, height) {
   return mass / (height**2) * 703;
 };
-
+//Write a function that converts temperature between celsius and fahrenheit
 var celsuisIndex = function(celsuis) {
   return (celsuis * 9/5) + 32;
 };
 
+//Write functions to be used by a cooking app that convert between units
 var fahrenheitIndex = function(fahrenheit) {
   return (fahrenheit - 32) * 5/9;
 };
 
-//Write a function that converts temperature between celsius and fahrenheit
 
 
-
-//Write functions to be used by a cooking app that convert between units
-
-
-
+// user interface
 $(document).ready(function() {
   $(".bmi").submit(function(event){
     event.preventDefault();
@@ -33,7 +25,6 @@ $(document).ready(function() {
     // alert(bmiIndex(mass, height));
     $('.output').text("Your BMI is " + bmiIndex(mass, height));
   });
-
 
  $(".celsius").submit(function(event){
    event.preventDefault();
